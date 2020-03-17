3af988406c3cf6b244a75bc38ae78d8bcb30fdb3
Yugantar Jain <yugantar8jain@gmail.com>
2020-03-17 17:40:49 +0530

    dash pro

---
+++ Agora/signupViewController.swift
@@ -0,0 +1,32 @@
+//
+//  signupViewController.swift
+//  Agora
+//
+//  Created by Yugantar Jain on 16/03/20.
+//  Copyright © 2020 Yugantar Jain. All rights reserved.
+//
+
+import UIKit
+
+class signupViewController: UIViewController {
+    
+    let questions = ["What was your childhood nickname", "In what city were you born", "What is your favourtie sport"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
