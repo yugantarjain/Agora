be947ed531fb1393f15231e690e2a301935e7e0c
Yugantar Jain <yugantar8jain@gmail.com>
2020-03-18 18:24:33 +0530

    ok

--- Agora/SecondViewController.swift
+++ Agora/SecondViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let titles = ["Agora UI Design", "Grand Grammy Election", "Class elections"]
+    let descriptions = ["To elect the best design for the Agora mobile app\nEnds Tomorrow at 3:00 pm", "The biggest user election to decide the best album of 2019", "Elections to choose the class representative"]
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(indexPath.row), for: indexPath)
+        
+        cell.textLabel?.text = titles[indexPath.row]
+        cell.detailTextLabel?.text = descriptions[indexPath.row]
+        
+        return cell
+    }
+    
 
+    @IBOutlet weak var table: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
     }
 
 
